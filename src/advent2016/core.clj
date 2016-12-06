ab2a0d1a7aba10bd91294805b8c035d34f6895f0
Adam Schmideg <offcode@holacrator.local>
2016-12-06 09:24:10 +0100

    aday 1

--- src/advent2016/core.clj
+++ src/advent2016/core.clj
@@ -1,6 +1,45 @@
-(ns advent2016.core)
+(ns advent2016.core
+  (:require [schema.core :as s]
+            [clojure.string :as string]
+            [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def Unit (s/enum -1 0 1))
+
+(def DirLoc
+  {:loc [s/Num s/Num]
+   :dir [Unit Unit]})
+
+(s/defn signum :- s/Int
+  [n :- s/Int]
+  (cond
+    (pos? n) 1
+    (zero? n) 0
+    :else -1))
+
+(s/defn step :- DirLoc
+  [dirloc :- DirLoc
+   step :- s/Num]
+  (let [[dx dy] (:dir dirloc)
+        [x y] (:loc dirloc)
+        [movex movey] (if (zero? dx) [(* step dy) 0]
+                                     [0 (- (* step dx))])]
+    {:loc [(+ x movex) (+ y movey)]
+     :dir [(signum movex) (signum movey)]}))
+
+(s/defn parse-step :- s/Int
+  [s :- s/Str]
+  (let [[dir & dist] s
+        dist (clojure.edn/read-string (apply str dist))]
+    (* dist (get {\R 1, \L -1} dir 0))))
+
+(s/defn move :- DirLoc
+  [s :- s/Str]
+  (let [steps (map (comp parse-step str/trim) (string/split s #","))]
+    (reduce step
+            {:loc [0 0] :dir [0 1]}
+            steps)))
+
+(defn d01
+  [s]
+  (let [[x y] (:loc (move s))]
+    (+ (Math/abs x) (Math/abs y))))
--- test/advent2016/core_test.clj
+++ test/advent2016/core_test.clj
@@ -1,7 +1,40 @@
 (ns advent2016.core-test
   (:require [clojure.test :refer :all]
+            [midje.sweet :refer :all]
             [advent2016.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(facts "d01"
+  (facts "signum"
+         (signum -5) => -1
+         (signum 0) => 0
+         (signum 8) => 1)
+  (facts "parse-step"
+         (parse-step "R3") => 3
+         (parse-step "L12") => -12
+         (parse-step "X55") => 0)
+  (tabular
+    (fact "step loc"
+      (:loc (step {:dir ?dir :loc [0 0]} ?step)) => ?new-loc)
+    ?dir ?step ?new-loc
+    [0 1] 2 [2 0]
+    [1 0] 3 [0 -3]
+    [0 -1] 4 [-4 0]
+    [-1 0] 5 [0 5])
+  (tabular
+    (fact "move"
+       (:loc (move ?moves)) => ?loc)
+    ?moves ?loc
+    "R5" [5 0]
+    "L4" [-4 0]
+    "R1,R1" [1 -1])
+
+  (tabular
+    (fact "d01"
+          (d01 ?path) => ?distance)
+    ?path ?distance
+    "R2, L3" 5
+    "R2, R2, R2" 2
+    "R5, L5, R5, R3" 12))
+
+
+
